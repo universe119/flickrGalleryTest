135bed6bcbec162ea58c2640592873dd020978f3
universe119 <ruddh119@gmail.com>
2024-09-26 09:13:30 +0900

    갤러리 타입 변경

--- js/flickr.js
+++ js/flickr.js
@@ -1,6 +1,11 @@
-fetchFlickr("interest");
+const [btnMine, btnPopular] = document.querySelectorAll("nav button");
 
-//body요소에 클릭했을때 클릭한요소의 클래스명을 구분자로 설정
+//스크립트 처음 로드시에는 내갤러리 출력
+fetchFlickr("mine");
+
+//각 버튼 클릭시 갤러 타입 변경
+btnMine.addEventListener("click", () => fetchFlickr("mine"));
+btnPopular.addEventListener("click", () => fetchFlickr("interest"));
 
 //특정 요소에 특정 함수 연결
 document.body.addEventListener("click", (e) => {
@@ -15,15 +20,12 @@ function fetchFlickr(type) {
 	const baseURL = `https://www.flickr.com/services/rest/?api_key=${api_key}&method=`;
 	const myID = "197119297@N02";
 	const method_mine = "flickr.people.getPhotos";
-	//method_interest는 flickr자체적으로 인기가 많은 이미지를 출력하는 메서드
+
 	const method_interest = "flickr.interestingness.getList";
 
-	//아래 2가지 타입의 요청 url생성  url_mine(내갤러리 호출url), url_interest(플리커 인기이미지 호출url)
 	let url_mine = `${baseURL}${method_mine}&user_id=${myID}&nojsoncallback=1&format=json`;
 	let url_interest = `${baseURL}${method_interest}&nojsoncallback=1&format=json`;
 
-	//fetchFlickr함수 호출시 파라미터로 전달된 인수값이 mine이면 마이갤러리 요청 url을 fetch함수에 전달
-	//그렇지 않으면 인기이미지 요청 url을 fetch함수에 전달
 	let result_url = type === "mine" ? url_mine : url_interest;
 
 	fetch(result_url)
