18da9c9842e836cd2fc776e0155a823b680f5bc4
universe119 <ruddh119@gmail.com>
2024-09-26 11:17:18 +0900

    fetch함수의 파라미터로 전달되는 인수값을 객체형태로 변환

--- js/flickr.js
+++ js/flickr.js
@@ -1,55 +1,47 @@
 const [btnMine, btnPopular] = document.querySelectorAll("nav button");
 let dataType = "";
 
-//스크립트 처음 로드시에는 내갤러리 출력
-fetchFlickr("mine");
+//fetch 함수 호출시 인수값을 객체 형태로 전달
+//이유: search, user타입 갤러리는 타입외에도 유저명, 검색어등의 추가 정보값을 제공해야 되기 때문
+fetchFlickr({ type: "mine" });
 
 //각 버튼 클릭시 갤러리 타입 변경
-btnMine.addEventListener("click", () => fetchFlickr("mine"));
-btnPopular.addEventListener("click", () => fetchFlickr("interest"));
+btnMine.addEventListener("click", () => fetchFlickr({ type: "mine" }));
+btnPopular.addEventListener("click", () => fetchFlickr({ type: "interest" }));
 
 //특정 요소에 특정 함수 연결
 document.body.addEventListener("click", (e) => {
 	if (e.target.className === "thumb") createModal(e);
 	if (e.target.className === "btnClose") removeModal();
 	//목록에서 사용자 프로필 아이디 클릭시 클릭한 span요소의 사용자 아이디 텍스트값을 fetchFlickr의 인수로 전달해 호출
-	if (e.target.className === "userID") fetchFlickr(e.target.innerText);
+	if (e.target.className === "userID")
+		fetchFlickr({ type: "user", userID: e.target.innerText }); //user타입 갤러리에는 userID라는 추가 프로퍼티로 유저아이디 값을 전달
 });
 
-//flickr fetching함수
-function fetchFlickr(type) {
-	//인수로 전달된 type정보와 현재 출력되고 있는 dataType이 동일하면
-	// 다시 data fetching할 필요가 없으므로 return으로 강제 함수 종료
-	// 만약 타입이 다르면 해당 if문 무시
-	if (type === dataType) return;
-	//인수로 전달된 타입명으로 현재 dataType을 변경
-	dataType = type;
+function fetchFlickr(opt) {
+	// 타입이 opt라는 파라미터 객체안쪽에 들어가 있기 때문에
+	if (opt.type === dataType) return;
+	dataType = opt.type;
 
 	const api_key = "21e294ad0ec03a32d7355980457d9e11";
 	const baseURL = `https://www.flickr.com/services/rest/?api_key=${api_key}&method=`;
 	const myID = "197119297@N02";
 	const method_mine = "flickr.people.getPhotos";
-
 	const method_interest = "flickr.interestingness.getList";
-
 	let url_mine = `${baseURL}${method_mine}&user_id=${myID}&nojsoncallback=1&format=json`;
-	//type으로 특정사용자의 ID값을 전달 받은뒤 user_id쿼리에 연결하면 해당 사용자의 갤러리를 호출하는 요청 URL생성
-	let url_user = `${baseURL}${method_mine}&user_id=${type}&nojsoncallback=1&format=json`;
-
+	let url_user = `${baseURL}${method_mine}&user_id=${opt.userID}&nojsoncallback=1&format=json`;
+	let url_search = `${baseURL}${method_mine}&tags=${opt.type}&nojsoncallback=1&format=json`;
 	let url_interest = `${baseURL}${method_interest}&nojsoncallback=1&format=json`;
 	let result_url = "";
-	// &&연산자 뒤에 변수에 값을 할당하는 대입연산자를 사용하고 싶을때는
-	// 대입식을 괄호로 묶어서 표현식으로 변경
-	if (type === "mine") result_url = url_mine;
-	else if (type === "interest") result_url = url_interest;
-	//type이 mine, interest가 아니면 특정 사용자 ID일테니 url_user요청 아이디를 result_url에 담아서 fetch함수에 전달
-	else result_url = url_user;
+	if (opt.type === "mine") result_url = url_mine;
+	else if (opt.type === "interest") result_url = url_interest;
+	else if (opt.type.indexOf("search*"));
+	else if (opt.type === user) result_url = url_user;
 
 	fetch(result_url)
 		.then((data) => data.json())
 		.then((json) => {
 			const picArr = json.photos.photo;
-
 			createList(picArr);
 		});
 }
