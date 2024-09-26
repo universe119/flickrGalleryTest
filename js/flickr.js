a425559a0b1c9356283407d08d5791aa832e7d5b
universe119 <ruddh119@gmail.com>
2024-09-26 13:15:15 +0900

    검색어 미입력후 검색버튼 클릭시 함수 강제 종료

--- js/flickr.js
+++ js/flickr.js
@@ -1,35 +1,27 @@
 let dataType = "";
 const [btnMine, btnPopular] = document.querySelectorAll("nav button");
-//searchBox안쪽에 있는 두번째 요소인 input, 세번째 요소인 btnSearch를 비구조할당으로 변수할당
 const [_, inputSearch, btnSearch] =
 	document.querySelector(".searchBox").children;
-// console.log(btnSearch); // input확인
 
-//fetch 함수 호출시 인수값을 객체 형태로 전달
-//이유: search, user타입 갤러리는 타입외에도 유저명, 검색어등의 추가 정보값을 제공해야 되기 때문
 fetchFlickr({ type: "mine" });
 
-//각 버튼 클릭시 갤러리 타입 변경
 btnMine.addEventListener("click", () => fetchFlickr({ type: "mine" }));
 btnPopular.addEventListener("click", () => fetchFlickr({ type: "interest" }));
-//검색버튼 클릭시
 btnSearch.addEventListener("click", () => {
-	//인풋요소의 value값 (검색어)을 tags에 담아 fetchFlickr함수 호출
+	//검색어를 입력하지 않고 검색버튼 클릭시 함수 강제 중지
+	if (!inputSearch.value) return;
 	fetchFlickr({ type: "search", tags: inputSearch.value });
-	//호출시 input요소의 검색어는 지워줌
 	inputSearch.value = "";
 });
-//특정 요소에 특정 함수 연결
+
 document.body.addEventListener("click", (e) => {
 	if (e.target.className === "thumb") createModal(e);
 	if (e.target.className === "btnClose") removeModal();
-	//목록에서 사용자 프로필 아이디 클릭시 클릭한 span요소의 사용자 아이디 텍스트값을 fetchFlickr의 인수로 전달해 호출
 	if (e.target.className === "userID")
-		fetchFlickr({ type: "user", userID: e.target.innerText }); //user타입 갤러리에는 userID라는 추가 프로퍼티로 유저아이디 값을 전달
+		fetchFlickr({ type: "user", userID: e.target.innerText });
 });
 
 function fetchFlickr(opt) {
-	// 타입이 opt라는 파라미터 객체안쪽에 들어가 있기 때문에
 	if (opt.type === dataType) return;
 	dataType = opt.type;
 
@@ -38,19 +30,16 @@ function fetchFlickr(opt) {
 	const myID = "197119297@N02";
 	const method_mine = "flickr.people.getPhotos";
 	const method_interest = "flickr.interestingness.getList";
-	// 검색전용 api메서드 추가
 	const method_search = "flickr.photos.search";
 	let url_mine = `${baseURL}${method_mine}&user_id=${myID}&nojsoncallback=1&format=json`;
 	let url_user = `${baseURL}${method_mine}&user_id=${opt.userID}&nojsoncallback=1&format=json`;
 	let url_interest = `${baseURL}${method_interest}&nojsoncallback=1&format=json`;
-	//opt로 전달된 tags 프로퍼티의 값을 tags라는 쿼리값 연동
 	let url_search = `${baseURL}${method_search}&tags=${opt.tags}&nojsoncallback=1&format=json`;
 	let result_url = "";
 
 	if (opt.type === "mine") result_url = url_mine;
 	if (opt.type === "interest") result_url = url_interest;
 	if (opt.type === "user") result_url = url_user;
-	// 전달된 opt.type값이 search이면 url_search 요청을 fetch함수에 전달
 	if (opt.type === "search") result_url = url_search;
 
 	fetch(result_url)
@@ -60,15 +49,12 @@ function fetchFlickr(opt) {
 			createList(picArr);
 		});
 }
-//body요소에 클릭했을때 클릭한요소의 클래스명을 구분자로 설정
-//특정 요소에 특정 함수 연결
 document.body.addEventListener("click", (e) => {
 	if (e.target.className === "thumb") createModal(e);
 	if (e.target.className === "btnClose") removeModal();
 	if (e.target.className === "userID") fetchFlickr(e.target.innerText);
 });
 
-//이미지 엑박시 대체이미지 연결 함수
 function setDefImg() {
 	const profilePic = document.querySelectorAll(".profile img"); //algarve sunset long exposure요 이미지가 엑박이므로 수정한거,
 	console.log(profilePic);
@@ -82,7 +68,6 @@ function setDefImg() {
 	);
 }
 
-//모달생성 함수
 function createModal(e) {
 	const imgSrc = e.target.getAttribute("alt");
 
@@ -98,12 +83,10 @@ function createModal(e) {
 	document.body.append(modal);
 }
 
-//모달 제거함수
 function removeModal() {
 	document.querySelector(".modal").remove();
 }
 
-//목록 생성 함수
 function createList(dataArr) {
 	const list = document.querySelector(".list");
 	let tags = "";
